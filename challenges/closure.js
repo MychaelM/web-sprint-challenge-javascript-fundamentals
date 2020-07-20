6f569e78a5a89846f2f932beec6137e367c790e5
Mychael M <mychaelbmenges@gmail.com>
2020-07-19 18:29:00 -0600

    finished prototypes tasks

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,27 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+  // nestedFunction can access internal variable because myFunction encloses nestedFunction and nestedFunction is able to reach out to the outer scope to access internal vatriable
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+// function summation(number) {
+
+//   let num = 1;
+
+//   return function counter() {
+
+//     for (let i = 0; i < number; i++) {
+//       num += num + num;
+//     }
+//     return num;
+//   };
+
+// }
+
+
+// let counter = summation(10);
+// console.log(summation(10));
+// console.log(counter());
